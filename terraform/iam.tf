d0cfd81fb7fd79dfab3b8aa7eba708c59c6615b3
Justin Reeves <reeves122@gmail.com>
2023-02-01 15:48:25 -0600

    update sqs permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "this" {
   statement {
     actions = [
-      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
       "sqs:GetQueueAttributes",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
