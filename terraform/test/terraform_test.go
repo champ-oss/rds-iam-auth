92c0a3f41883cff53314c71441c9bc73ab652907
Justin Reeves <reeves122@gmail.com>
2023-02-01 10:08:33 -0600

    use state for terraform testing

--- terraform/test/terraform_test.go
+++ terraform/test/terraform_test.go
@@ -2,24 +2,19 @@ package test
 
 import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"os/exec"
+	"os"
 	"testing"
 )
 
 func TestTerraform(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
-		TerraformDir:  "../examples/complete",
-		BackendConfig: map[string]interface{}{},
-		Vars:          map[string]interface{}{},
+		TerraformDir: "../examples/complete",
+		BackendConfig: map[string]interface{}{
+			"bucket": os.Getenv("TF_STATE_BUCKET"),
+			"key":    os.Getenv("TF_VAR_git"),
+		},
+		Vars: map[string]interface{}{},
 	}
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.Init(t, terraformOptions)
-
-	// recursively set prevent destroy to false
-	cmd := exec.Command("bash", "-c", "find . -type f -name '*.tf' -exec sed -i'' -e 's/prevent_destroy = true/prevent_destroy = false/g' {} +")
-	cmd.Dir = "../../"
-	_ = cmd.Run()
-
-	terraform.ApplyAndIdempotent(t, terraformOptions)
+	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
 }
