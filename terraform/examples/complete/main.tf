95bfa8659571f6ee6dcfa92c1eab91ec77b19652
Justin Reeves <reeves122@gmail.com>
2023-02-01 12:02:17 -0600

    create aurora cluster

--- terraform/examples/complete/main.tf
+++ terraform/examples/complete/main.tf
@@ -6,6 +6,50 @@ provider "aws" {
   region = "us-east-2"
 }
 
+locals {
+  git = "rds-iam-auth"
+  tags = {
+    cost    = "shared"
+    creator = "terraform"
+    git     = local.git
+  }
+}
+
+data "aws_vpcs" "this" {
+  tags = {
+    purpose = "vega"
+  }
+}
+
+data "aws_subnets" "private" {
+  tags = {
+    purpose = "vega"
+    Type    = "Private"
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpcs.this.ids[0]]
+  }
+}
+
+resource "aws_security_group" "test" {
+  name_prefix = "test-aurora-"
+  vpc_id      = data.aws_vpcs.this.ids[0]
+}
+
+module "aurora" {
+  source                    = "github.com/champ-oss/terraform-aws-aurora.git?ref=v1.0.29-f57eb21"
+  cluster_identifier_prefix = local.git
+  git                       = local.git
+  protect                   = false
+  skip_final_snapshot       = true
+  vpc_id                    = data.aws_vpcs.this.ids[0]
+  private_subnet_ids        = data.aws_subnets.private.ids
+  source_security_group_id  = aws_security_group.test.id
+  tags                      = local.tags
+}
+
 module "this" {
   source = "../../"
 }
