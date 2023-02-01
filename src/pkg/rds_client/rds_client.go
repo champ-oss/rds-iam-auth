2f6ca886cd449ea1ffeb0979db2aaefe0e6d5969
Justin Reeves <reeves122@gmail.com>
2023-02-01 11:55:31 -0600

    update scheduler to get all rds clusters

--- src/pkg/rds_client/rds_client.go
+++ src/pkg/rds_client/rds_client.go
@@ -6,34 +6,42 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 	"github.com/champ-oss/rds-iam-auth/pkg/common"
+	log "github.com/sirupsen/logrus"
 )
 
 type RdsClient struct {
+	region    string
 	queueUrl  string
 	rdsClient *rds.Client
 }
 
 func NewRdsClient(region string, queueUrl string) *RdsClient {
 	return &RdsClient{
+		region:    region,
 		queueUrl:  queueUrl,
 		rdsClient: rds.NewFromConfig(common.GetAWSConfig(region)),
 	}
 }
 
-func (r *RdsClient) GetAllDatabases() {
+func (r *RdsClient) GetAllDatabases() []types.DBCluster {
 	var dbClusters []types.DBCluster
 
+	log.Infof("getting list of RDS clusters in region: %s", r.region)
 	paginator := rds.NewDescribeDBClustersPaginator(r.rdsClient, &rds.DescribeDBClustersInput{})
 	for paginator.HasMorePages() {
 		page, err := paginator.NextPage(context.TODO())
 		if err != nil {
 			fmt.Printf("failed to get a page, %w", err)
 		}
+		log.Debugf("retrieved %d items", len(page.DBClusters))
 
 		for _, dbCluster := range page.DBClusters {
-			fmt.Println(*dbCluster.DBClusterArn)
+			log.Debug(*dbCluster.DBClusterIdentifier)
 		}
 
 		dbClusters = append(dbClusters, page.DBClusters...)
 	}
+
+	log.Infof("found %d RDS clusters", len(dbClusters))
+	return dbClusters
 }
--- src/service/scheduler/scheduler.go
+++ src/service/scheduler/scheduler.go
@@ -2,21 +2,25 @@ package scheduler
 
 import (
 	cfg "github.com/champ-oss/rds-iam-auth/config"
+	"github.com/champ-oss/rds-iam-auth/pkg/rds_client"
 	"github.com/champ-oss/rds-iam-auth/pkg/sqs_client"
 )
 
 type Service struct {
 	config    *cfg.Config
 	sqsClient *sqs_client.SqsClient
+	rdsClient *rds_client.RdsClient
 }
 
 func NewService(config *cfg.Config) *Service {
 	return &Service{
 		config:    config,
 		sqsClient: sqs_client.NewSqsClient(config.AwsRegion, config.QueueUrl),
+		rdsClient: rds_client.NewRdsClient(config.AwsRegion, config.QueueUrl),
 	}
 }
 
 func (s *Service) Run() error {
+	_ = s.rdsClient.GetAllDatabases()
 	return nil
 }
