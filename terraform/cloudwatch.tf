037f6b6885ebcb212f3aa976bc75e43ab6626aab
Justin Reeves <reeves122@gmail.com>
2023-03-13 16:47:58 -0500

    comment event detail types

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -5,12 +5,12 @@ resource "aws_cloudwatch_event_rule" "this" {
     "source" : [
       "aws.rds"
     ],
-    detail-type = [
-      "RDS DB Snapshot Event",
-      "RDS DB Instance Event",
-      "RDS DB Cluster Event",
-      "RDS DB Snapshot Event"
-    ]
+    #    detail-type = [
+    #      "RDS DB Snapshot Event",
+    #      "RDS DB Instance Event",
+    #      "RDS DB Cluster Event",
+    #      "RDS DB Snapshot Event"
+    #    ]
   })
 }
 
