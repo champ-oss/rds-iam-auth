ca7122f3cae66909d052b6fb5c39d408f005fa52
Justin Reeves <reeves122@gmail.com>
2023-12-19 09:17:21 -0600

    flatten terraform files. remove terratest.

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,7 +1,3 @@
-terraform {
-  backend "s3" {}
-}
-
 provider "aws" {
   region = "us-east-2"
 }
@@ -89,4 +85,56 @@ module "this" {
   schedule_expression = "cron(0 4 * * ? *)"
   retry_delay_seconds = 30
   max_receive_count   = 60
+}
+
+output "region" {
+  description = "AWS region"
+  value       = module.this.region
+}
+
+output "test_mysql_endpoint" {
+  description = "MySQL endpoint"
+  value       = module.mysql.address
+}
+
+output "test_mysql_master_username" {
+  description = "MySQL master user"
+  value       = module.mysql.master_username
+}
+
+output "test_mysql_master_password" {
+  description = "MySQL master password"
+  value       = module.mysql.password
+  sensitive   = true
+}
+
+output "test_aurora_endpoint" {
+  description = "Aurora endpoint"
+  value       = module.aurora.endpoint
+}
+
+output "test_aurora_master_username" {
+  description = "Aurora master user"
+  value       = module.aurora.master_username
+}
+
+output "test_aurora_master_password" {
+  description = "Aurora master password"
+  value       = module.aurora.master_password
+  sensitive   = true
+}
+
+output "function_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function#function_name"
+  value       = module.this.function_name
+}
+
+output "db_iam_read_username" {
+  description = "read only user"
+  value       = module.this.db_iam_read_username
+}
+
+output "db_iam_admin_username" {
+  description = "admin user"
+  value       = module.this.db_iam_admin_username
 }
