08a87dc325b8e883dbf4b13d8e149f146321cf6a
Justin Reeves <reeves122@gmail.com>
2023-12-19 10:01:28 -0600

    update terraform modules

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_region" "this" {}
 data "aws_caller_identity" "this" {}
 
 module "hash" {
-  source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.13-ffd1b7d"
+  source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.14-02da677"
   path     = path.module
   fallback = ""
 }
